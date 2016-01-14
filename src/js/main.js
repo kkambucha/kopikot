23cbc1c771816d9339789c600a9e6de28483eaf9
Vadim Zakovryashin <vadim.zakovryashin@starcode.ru>
2016-01-14 11:40:24 +0300

    Верстка 2-х страниц

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,13 @@
  */
 /* //= ../../bower_components/jquery/dist/jquery.js */
 
+//= partials/jquery-1.12.0.js
+
+//= partials/jquery-validation/jquery.validate.js
+
+//= partials/jquery-validation/additional-methods.js
+
+//= partials/jquery-validation/localization/messages_ru.js
 
 /*
  * Custom
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,47 @@
-(function(){
-    console.log('it\'s work');
-})();
+$(document).ready(function(){
+
+	function appendBgBlock(){
+		var contentHeight = $('.l-centercol').height(),
+			contentLeftOffset = $('.l-centercol').offset().left;
+
+		$('.h-background').css({
+			position: 'absolute',
+			width: contentLeftOffset + 'px',
+			height: contentHeight + 'px',
+			top: 0,
+			left: 0,
+			marginLeft: -contentLeftOffset + 'px',
+		});
+	}
+
+	appendBgBlock();
+
+	$(window).resize(function(){
+		appendBgBlock();	
+	});
+	
+	if($('#post-form').length > 0){
+		$('#post-form').validate({
+			invalidHandler: function(event, validator) {
+		    // 'this' refers to the form
+		    var errors = validator.numberOfInvalids();
+		    if (errors) {
+		      var message = errors == 1
+		        ? 'You missed 1 field. It has been highlighted'
+		        : 'You missed ' + errors + ' fields. They have been highlighted';
+		      $("div.error span").html(message);
+		      $("div.error").show();
+		    } else {
+		      $("div.error").hide();
+		    }
+		  },
+		  rules: {
+		    email: {
+		      required: true,
+		      email: true
+		    }
+		  }
+		});
+	}
+
+});
